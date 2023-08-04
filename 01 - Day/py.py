5e7b8a2dccd3c1b1db86b7e63df5ca927364b70a
Yashoda <yashodalakshmi999@gmail.com>
2023-08-04 21:34:16 +0530

    Python Day - 01 completed

---
+++ 01 - Day/py.py
@@ -0,0 +1,24 @@
+# Print Statement :
+
+print("Python");
+
+# Calculations :
+
+print(3 + 2);
+print(3 - 2);
+print(3 * 2);
+print(3 / 2);
+print(int(3 / 2));
+print(3 % 2);
+
+# Assigning Variables :
+
+a = "Hare";
+b = "Krishna";
+c = "Rama";
+print(a + " " + b + " " + c);
+print(a + "" + b + "" + c);
+
+
+# a = input("Enter your Name : ");
+#print("Your Name is : " + a);
