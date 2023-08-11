f9029eb96b3441ae3a48c76927a4b53a108f4695
Yashoda <yashodalakshmi999@gmail.com>
2023-08-11 16:35:57 +0530

    Day1 completed

---
+++ 01 - Day/day1.py
@@ -0,0 +1,45 @@
+# Print Statement :
+print("Python");
+
+# Calculations :
+
+print(3 + 2);
+print(3 - 2);
+print(3 * 2);
+print(3 / 2);
+print(int(3 / 2));
+print(3 % 2);
+
+# Assigning Variables :
+
+a = "Hare";
+b = "Krishna";
+c = "Rama";
+print(a + " " + b + " " + c);
+print(a + "" + b + "" + c);
+
+
+a = input("Enter your Name : ");
+print("Your Name is : " + a);
+
+
+# Questions to Practice (Day 1):
+
+# 1. Print your name.
+print("Yashoda V")
+
+#2. Print the result of adding two numbers.
+a = 4
+b = 2
+print(a + b)
+
+#3. Print the result of subtracting two numbers.
+print(a - b)
+
+#4. Print the result of multiplying two numbers.
+print(a * b)
+
+#5. Print the result of dividing two numbers.
+print(a / b)
+
+print(int(a / b))
