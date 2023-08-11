b4c680aeab1e6fe446d56992cbdb88a8e0714fd8
Yashoda <yashodalakshmi999@gmail.com>
2023-08-11 23:03:50 +0530

    Day - 07 Completed

---
+++ 07 - Day/day7.py
@@ -0,0 +1,128 @@
+x = 5
+y = 7
+print("Example - 01")
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # False
+print(x < y)    # True
+print(x >= y)   # False
+print(x <= y)   # True
+
+print("Example - 02")
+a = 5
+b = 2-7
+
+print(a == b)  
+print(a != b)   
+print(a > b)   
+print(a < b)   
+print(a >= b)
+print(a <= b)
+
+### Questions to practice (Day 7):
+
+# 1. Example: Greater than operator (>)
+# Input: 5 > 3
+# Output: True
+
+print("Question No : 01")
+print("5 > 3 is :", 5 > 3)
+
+# 2. Example: Less than operator (<)
+# Input: 10 < 20
+# Output: True
+
+print("Question No : 02")
+print("10 < 20 is :", 10 < 20)
+
+# 3. Example: Greater than or equal to operator (>=)
+# Input: 7 >= 7
+# Output: True
+
+print("Question No : 03")
+print("7 >= 7 is :", 7 >= 7)
+
+# 4. Example: Less than or equal to operator (<=)
+# Input: 15 <= 12
+# Output: False
+
+print("Question No : 04")
+print("15 <= 12 is :", 15 <= 12)
+
+# 5. Example: Equal to operator (==)
+# Input: "hello" == "hello"
+# Output: True
+
+print("Question No : 05")
+print('"hello" == "hello" is :', "hello" == "hello")
+
+# 6. Example: Not equal to operator (!=)
+# Input: 10 != 20
+# Output: True
+
+print("Question No : 06")
+print("10 != 20 is :", 10 != 20)
+
+# 7. Example: Comparing integers and floats
+# Input: 5.0 > 4
+# Output: True
+
+print("Question No : 07")
+print("5.0 > 4 is :", 5.0 > 4)
+
+# 8. Example: Comparing strings with different cases
+# Input: "Hello" == "hello"
+# Output: False
+
+print("Question No : 08")
+print('"Hello" == "hello" is :', "Hello" == "hello")
+
+# 9. Example: Using relational operators with booleans
+# Input: True == False
+# Output: False
+
+print("Question No : 09")
+print('True == False is :', True == False)
+
+# 10. Example: Comparing strings
+# Input: "apple" < "banana"
+# Output: True
+
+print("Question No : 10")
+print('"apple" < "banana" is :', "apple" < "banana")
+
+# 11. Example: Comparing None with a string
+# Input: None == "Python"
+# Output: False
+
+print("Question No : 11")
+print('None == "Python" is :', None == "Python")
+
+# 12. Example: Mixing different types in comparisons
+# Input: 5 > "3"
+# Output: TypeError
+
+print("Question No : 12")
+# print('5 > "3" is :', 5 > "3")
+
+# 13. Example: Using relational operators with negative numbers
+# Input: -10 < -5
+# Output: True
+
+print("Question No : 13")
+print('-10 < -5 is :', -10 < -5)
+
+# 14. Example: Comparing a string with a number
+# Input: "42" == 42
+# Output: False
+
+print("Question No : 14")
+print('"42" == 42 is :', "42" == 42)
+
+# 15. Example: Using relational operators with floating-point precision
+# Input: 0.1 + 0.1 + 0.1 == 0.3
+# Output: False
+
+print("Question No : 15")
+print('0.1 + 0.1 + 0.1 == 0.3 is :', 0.1 + 0.1 + 0.1 == 0.3)
+print('0.3 == 0.3 is :', 0.3 == 0.3)
