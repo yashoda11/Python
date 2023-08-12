1d53199aee4e992da6765ef92bc6c570dcbd3c9b
Yashoda <yashodalakshmi999@gmail.com>
2023-08-13 00:27:29 +0530

    Day - 09 Completed

---
+++ 09 - Day/Q3.py
@@ -0,0 +1,12 @@
+# **Question 3:**
+# Write a program that takes a character as input and prints "Vowel" if it's a vowel (a, e, i, o, u), and "Consonant" otherwise.
+# **Expected Input:** 
+# Enter a character : a
+# **Expected Output:** Vowel
+
+x = input("Enetr a Character : ")
+
+if ( x == "a" or x == "e" or x == "i" or x == "o" or x == "u") :
+    print("Vowel");
+else :
+    print("Consonant")
