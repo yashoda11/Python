0975378d17583f3f10a008eca535521a39949f00
Yashoda <yashodalakshmi999@gmail.com>
2023-08-14 11:23:18 +0530

    Day - 11

---
+++ 11 - Day/day11.py
@@ -0,0 +1,21 @@
+# while condition:
+     # Code block to be executed while the condition is True
+
+# Example: Printing numbers from 1 to 5 using a while loop
+
+num = 1
+
+while num <= 5 :
+    print (num)    
+    num += 1
+
+# Example: Finding the sum of numbers from 1 to 10 using a while loop
+
+num = 1
+sum = 0
+
+while num <= 10:
+    sum += num
+    num += 1
+print ("Sum of Numners from 1 to 10 is :", sum)
+
