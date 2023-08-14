578eb1b3d8c332d4deb8a117dfbf027209b4417e
Yashoda <yashodalakshmi999@gmail.com>
2023-08-14 11:25:04 +0530

    Question - 01

---
+++ 11 - Day/Q1.py
@@ -0,0 +1,19 @@
+# Question 1: # Write a while loop that prints numbers from 1 to 10.
+# Expected Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
+    
