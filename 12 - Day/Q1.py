90e2cbab8df82221ea063ed17196e53f46a2a50d
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 05:24:18 +0530

    Question - 01

---
+++ 12 - Day/Q1.py
@@ -0,0 +1,12 @@
+# 1. Write a for loop that prints all numbers from 1 to 5.
+# Input: None
+# Expected Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+print("All numbers from 1 to 5 :")
+for num in range (1 , 6) :
+    print(num)
