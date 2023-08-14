1092dbcf9d9dc001fdcd9efcf78cc80c1becb866
Yashoda <yashodalakshmi999@gmail.com>
2023-08-14 11:33:38 +0530

    Question - 03

---
+++ 11 - Day/Q3.py
@@ -0,0 +1,15 @@
+# Question 3: Write a while loop that prints even numbers from 2 to 10.
+# Expected Output:
+# 2
+# 4
+# 6
+# 8
+# 10
+
+num = 2
+
+print("The Even Numbers from 2 to 10 are : ")
+while num <= 10 :
+    print(num)
+    num += 2
+
