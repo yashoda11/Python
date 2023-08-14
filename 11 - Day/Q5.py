76cc04ae2d9bee7692bbfd11113074c50f132f38
Yashoda <yashodalakshmi999@gmail.com>
2023-08-14 11:42:10 +0530

    Question - 05

---
+++ 11 - Day/Q5.py
@@ -0,0 +1,18 @@
+# Question 5: # Write a while loop that counts down from 10 to 1.
+# Expected Output:
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+
+number = 10
+
+while number >= 1 :
+    print(number)
+    number -= 1
