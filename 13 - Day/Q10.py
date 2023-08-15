2c4047698c527f79a4e5dfbe9f798de7b2572cc3
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:36:30 +0530

    Question - 10

---
+++ 13 - Day/Q10.py
@@ -0,0 +1,7 @@
+# 10. Given a string "banana", count the occurrences of the letter "a" using the count() method.
+# Input: "banana"
+# Expected Output: 3
+
+string = "banana"
+result = string.count("a")
+print(result)
