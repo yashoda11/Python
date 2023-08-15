44cdec9f4124895c93a7c36ade5f325d0e0d11c4
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:14:55 +0530

    Question - 03

---
+++ 13 - Day/Q3.py
@@ -0,0 +1,9 @@
+# 3. Write a Python function that takes a string as input and converts it to uppercase using the upper() method.
+# Input: 
+# "Hello, World!"
+# Expected Output: 
+# "HELLO, WORLD!"
+
+string = input("Enter a String : ")
+upper_case = string.upper()
+print("The Upper Case of String", string, "is : ", upper_case)
