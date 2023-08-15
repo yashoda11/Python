e21b4d9327487eca7f3167df0ccd739cf5d7d969
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 05:44:16 +0530

    Question - 05

---
+++ 12 - Day/Q5.py
@@ -0,0 +1,11 @@
+# 2. Create a for loop that doubles each number in the sequence: 1, 2, 3, 4, 5.
+# Input: None
+# Expected Output:
+# 2
+# 4
+# 6
+# 8
+# 10
+
+for num in range(1,6) :
+    print (num*2)
