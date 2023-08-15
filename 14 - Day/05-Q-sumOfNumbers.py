f1bd291b121f846a2ae0096f8ea4478d97b0b16f
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 08:57:47 +0530

    Day-14

---
+++ 14 - Day/05-Q-sumOfNumbers.py
@@ -0,0 +1,8 @@
+# 5. Write a nested loop to calculate and print the sum of numbers from 1 to 5 using nested iteration.
+# Expected Input: None
+# Expected Output: 15
+
+sum = 0
+for i in range (1,6):
+    sum +=i
+print(sum)
---
+++ 14 - Day/06-Q-Reverse-Multiplication.py
@@ -0,0 +1,8 @@
+Create a nested loop to print the reverse of the multiplication table from 1 to 5 (up to 10 times each).
+Expected Input: None
+Expected Output:    
+5  4  3  2  1
+10 8  6  4  2
+15 12 9  6  3
+20 16 12 8  4
+25 20 15 10 5
