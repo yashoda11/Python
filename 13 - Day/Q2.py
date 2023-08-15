4114437f2d74744337c6d28d8ec3242cb06f5171
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:11:53 +0530

    Question - 01

---
+++ 13 - Day/Q2.py
@@ -0,0 +1,7 @@
+# 2. Given a string "Hello, World!", find its length using the len() method.
+# Input: "Hello, World!"
+# Expected Output: 13
+
+string = input("Enter a String : ")
+length = len(string)
+print("The Length of String", string, "is :",length)
