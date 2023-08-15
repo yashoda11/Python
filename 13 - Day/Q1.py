6a536eecf3bd10bf5176878bddc14ff5bdd3fcfd
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:09:18 +0530

    Question - 01

---
+++ 13 - Day/Q1.py
@@ -0,0 +1,7 @@
+# 1. Write a Python program that takes a user input string and converts it to lowercase using the lower() method.
+# Input: "Hello, World!"
+# Expected Output: "hello, world!"
+
+string = input("Enter a String : ")
+lower_case = string.lower()
+print("The Lower Case of String", string, "is :", lower_case)
