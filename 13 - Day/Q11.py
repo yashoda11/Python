a17cd0af304cc5236f948489f74b648fff41cc16
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:38:41 +0530

    Question - 11

---
+++ 13 - Day/Q11.py
@@ -0,0 +1,7 @@
+# 11. Write a Python program that takes a user input string and finds the index of the first occurrence of the letter "l" using the find() method.
+# Input: "Hello, World!"
+# Expected Output: 2
+
+string = input("Enter a String : ")
+find_string_index = string.find("l")
+print(find_string_index)
