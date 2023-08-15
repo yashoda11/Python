6b52d77da252c4e99e3f325603acd54e4f7900e6
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:34:22 +0530

    Question - 09

---
+++ 13 - Day/Q9.py
@@ -0,0 +1,7 @@
+# 9. Write a Python function that takes a string as input and checks if it ends with the word "end" using the endswith() method.
+# Input: "Hello, World!"
+# Expected Output: False
+
+string = input("Enter a String : ")
+isString_endswith = string.endswith("end")
+print(isString_endswith)
