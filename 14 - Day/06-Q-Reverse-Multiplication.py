470dbce88b5d79cb0731247e314e340923ebed9f
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 09:10:43 +0530

    Question - 06

--- 14 - Day/06-Q-Reverse-Multiplication.py
+++ 14 - Day/06-Q-Reverse-Multiplication.py
@@ -1,8 +1,14 @@
-Create a nested loop to print the reverse of the multiplication table from 1 to 5 (up to 10 times each).
-Expected Input: None
-Expected Output:    
-5  4  3  2  1
-10 8  6  4  2
-15 12 9  6  3
-20 16 12 8  4
-25 20 15 10 5
+# Create a nested loop to print the reverse of the multiplication table from 1 to 5 (up to 10 times each).
+# Expected Input: None
+# Expected Output:    
+# 5  4  3  2  1
+# 10 8  6  4  2
+# 15 12 9  6  3
+# 20 16 12 8  4
+# 25 20 15 10 5
+
+for i in range (1,6):
+    for j in range (5,0,-1) :
+        print(i*j, end="\t")
+    print()
+
