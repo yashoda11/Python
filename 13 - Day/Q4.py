fb6f82edb4db75bde0a2d6b82bd236b18cf8f5be
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:16:52 +0530

    Question - 04

---
+++ 13 - Day/Q4.py
@@ -0,0 +1,7 @@
+# 4. Given a string "Hello123", check if it contains only alphabetic characters using the isalpha() method.
+# Input: "Hello123"
+# Expected Output: False
+
+string = "Hello123"
+is_alpha = string.isalpha()
+print(is_alpha)
