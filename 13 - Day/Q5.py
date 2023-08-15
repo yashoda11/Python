af4adb18eef3432cd1621ee2e833b8a1036c39e5
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:20:59 +0530

    Question - 05

---
+++ 13 - Day/Q5.py
@@ -0,0 +1,7 @@
+# 5. Write a Python program that takes a user input string with leading and trailing whitespaces and removes them using the strip() method.
+# Input: " Python Programming "
+# Expected Output: "Python Programming"
+
+string = input("Enter a String : ")
+stripped = string.strip()
+print(stripped)
