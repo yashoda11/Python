2e7674262cae2c48b36e11f64058d0d6705a2c16
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:40:57 +0530

    Day - 12 Completed

---
+++ 13 - Day/Q12.py
@@ -0,0 +1,7 @@
+# 12. Given a string "12345", check if it consists of only digits using the isdigit() method.
+# Input: "12345"
+# Expected Output: True
+
+string = "12345"
+is_digit = string.isdigit()
+print(is_digit)
