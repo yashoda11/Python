d8048997052590c578b91cdb81a993c07fca8d77
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:30:56 +0530

    Question - 07

---
+++ 13 - Day/Q7.py
@@ -0,0 +1,7 @@
+# 7. Write a Python program that takes a user input string and replaces all occurrences of "Python" with "Java" using the replace() method.
+# Input: "I love Python programming, Python is great."
+# Expected Output: "I love Java programming, Java is great."
+
+string = "I love Python programming, Python is great."
+replace_string = string.replace("Python","Java")
+print(replace_string)
