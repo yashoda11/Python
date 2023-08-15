243b5a03c9bf9f470b2ae75750b0ace5392c8926
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 05:32:44 +0530

    Question - 03

---
+++ 12 - Day/Q3.py
@@ -0,0 +1,13 @@
+# 3. Write a for loop to print each character in the string "Hello".
+# Input: None
+# Expected Output:
+# H
+# e
+# l
+# l
+# o
+
+print("To print each character in the string 'Hello' is :")
+sequence = "Hello"
+for char in sequence:
+    print(char)
