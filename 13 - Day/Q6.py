8c4132dd37b3e482b009ae43b401e058508147c5
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:28:41 +0530

    Question - 06

---
+++ 13 - Day/Q6.py
@@ -0,0 +1,13 @@
+# Given a string "I love Python programming", split it into words using the split() method and print each word on a new line.
+# Input: "I love Python programming"
+# Expected Output:
+# I
+# love
+# Python
+# programming
+
+string = "I love Python programming"
+splitted = string.split()
+print(splitted)
+for word in splitted :
+    print(word)
