e6efdc23a05a948c34ed14a793a01fd60de36436
Yashoda <yashodalakshmi999@gmail.com>
2023-08-15 06:32:17 +0530

    Question - 08

---
+++ 13 - Day/Q8.py
@@ -0,0 +1,7 @@
+# 8. Given a string "Hello, World!", check if it starts with the word "Hello" using the startswith() method.
+# Input: "Hello, World!"
+# Expected Output: True
+
+string = "Hello, World!"
+isString_startswith = string.startswith("Hello")
+print(isString_startswith)
