f09034a6d4ceb03012abcf77f26216b0980997e0
Yashoda <yashodalakshmi999@gmail.com>
2023-08-24 23:32:16 +0530

    Day 22 Questions practice

--- 22 - Day - Working with Sets/day22.py
+++ 22 - Day - Working with Sets/day22.py
@@ -36,6 +36,7 @@ print("-----------------------------------------------------------------")
 # Expected Output: {1, 2, 4, 5}
 
 print("Question -01")
+print("Symmetric Difference between two sets :")
 setA = {1, 2, 3}
 setB = {3, 4, 5}
 symmetric_difference_set = setA.symmetric_difference(setB)
@@ -48,28 +49,58 @@ print("-----------------------------------------------------------------")
 # Expected Output: True
 
 print("Question -02")
-set = {1, 2, 3}
 print("Checking membership in a set :")
+set = {1, 2, 3}
 element_to_check = 2
 print("Checking whether the given element is exist or not.")
 print(element_to_check in set)
 
 print("-----------------------------------------------------------------")
 
-print("Question -03") 
-# question - 03
+# **Question 3: Union of two sets**
+# Expected Input: Set A: {1, 2, 3}, Set B: {3, 4, 5}
+# Expected Output: {1, 2, 3, 4, 5}
+
+print("Question - 03  -  Union of two sets") 
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+result = set1.union(set2)
+print("Union of two sets", set1, "and ", set2, "is :",result)
 
 print("-----------------------------------------------------------------")
 
+# **Question 4: Intersection of two sets**
+# Expected Input: Set A: {1, 2, 3}, Set B: {3, 4, 5}
+# Expected Output: {3}
 
+print("Question - 04  -  Intersection of two sets")
+set1 = {1, 2, 3}
+set2 = { 3, 4, 5}
+result = set1.intersection(set2)
+print("Intersection of two sets", set1, "and ", set2, "is :", result)
 
-print("Question -04")
 print("-----------------------------------------------------------------")
 
-print("Question -05")
+# **Question 5: Difference between two sets**
+# Expected Input: Set A: {1, 2, 3, 4}, Set B: {3, 4, 5}
+# Expected Output: {1, 2}
+
+print("Question - 05  -  Difference between two sets ")
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5}
+result = set1.difference(set2)
+print("Difference between two sets", set1, "and", set2, "is :", result)
 
 print("-----------------------------------------------------------------")
-print("Question -06")
+
+# **Question 6: Removing duplicate elements from a list using a set**
+# Expected Input: List: [1, 2, 2, 3, 4, 4, 5]
+# Expected Output: [1, 2, 3, 4, 5]
+
+# print("Question - 06  -  Removing duplicate elements from a list using a set ")
+# given_list = [1, 2, 2, 3, 4, 4, 5]
+# updated_
+# for elme
 
 print("-----------------------------------------------------------------")
 print("Question -07")
